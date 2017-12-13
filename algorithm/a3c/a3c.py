12d7240e70c72977727e042ebc2b8ce60cf0cf7b
Lance <nevertiree@126.com>
2017-12-13 15:16:26 +0800

    实现了Actor-Critic的Loss Graph，把Network_output改成了静态函数

--- algorithm/a3c/a3c.py
+++ algorithm/a3c/a3c.py
@@ -13,8 +13,9 @@ import matplotlib.pyplot as plt
 
 OUTPUT_GRAPH = True
 LOG_DIR = './log'  # 输出log目录
-N_WORKERS = multiprocessing.cpu_count()  # 并行work数量等于CPU核数
-MAX_EPISODE_TIME = 100000
+# N_WORKERS = multiprocessing.cpu_count()  # 并行work数量等于CPU核数
+N_WORKERS = 1
+MAX_EPISODE_TIME = 10000
 GLOBAL_NET_SCOPE = 'Global_Net'  # 是否为全局网络
 current_episode_time = 0
 MAX_STEP_NUM = 100
@@ -43,18 +44,19 @@ class Agent(object):
 
                 # 取build_net()的最后两个返回值
                 # 前两个返回值是action_prob和estimated_value，只有本地Agent需要
-                self.actor_params, self.critic_params = self._get_network_output(scope)[-2:]
+                self.actor_params, self.critic_params = self._get_network_output(self.state, scope)[-2:]
         # 本地Agent
         else:
             # 基础数据
             with tf.variable_scope(scope):
                 # 读入State、Action和Target_Value
                 self.state = tf.placeholder(tf.float32, [None, N_S, N_S], 'State')
-                self.action = tf.placeholder(tf.int32, [None, ], 'Action')
+                self.action = tf.placeholder(tf.int32, [None, 1], 'Action')
                 self.target_value = tf.placeholder(tf.float32, [None, 1], 'Target_Value')
 
                 # 取build_net()全部返回值
-                self.action_prob, self.estimated_value, self.actor_params, self.critic_params = self._get_network_output(scope)
+                self.action_prob, self.estimated_value, self.actor_params, self.critic_params = \
+                    self._get_network_output(self.state, scope)
 
                 self.actor_loss, self.critic_loss = \
                     Agent._get_loss_value(self.target_value, self.estimated_value, self.action, self.action_prob)
@@ -66,10 +68,10 @@ class Agent(object):
             with tf.name_scope('sync'):
                 # 从远程获取参数
                 with tf.name_scope('pull'):
-                    self.pull_actor_params_op = [l_p.assign(g_p) for l_p, g_p in
-                                                 zip(self.actor_params, global_ac.actor_params)]
-                    self.pull_c_params_op = [l_p.assign(g_p) for l_p, g_p in
-                                             zip(self.critic_params, global_ac.critic_params)]
+                    self.pull_actor_params_op = \
+                        [l_p.assign(g_p) for l_p, g_p in zip(self.actor_params, global_ac.actor_params)]
+                    self.pull_c_params_op = \
+                        [l_p.assign(g_p) for l_p, g_p in zip(self.critic_params, global_ac.critic_params)]
                 # 向远程提交数据
                 with tf.name_scope('push'):
                     self.update_actor_op = OPT_A.apply_gradients(zip(self.actor_grads, global_ac.actor_params))
@@ -81,12 +83,12 @@ class Agent(object):
         td_error = tf.subtract(target_value, estimated_value, name='TD_error')
 
         # 计算Critic loss
-        with tf.name_scope('c_loss'):
+        with tf.name_scope('critic_loss'):
             critic_loss = tf.reduce_mean(tf.square(td_error))  # TD Error的平方和平均
 
         # 计算Actor loss
         # todo 重点分析Loss Function
-        with tf.name_scope('a_loss'):
+        with tf.name_scope('actor_loss'):
             log_prob = tf.reduce_sum(tf.log(action_prob) * tf.one_hot(action, N_A, dtype=tf.float32),
                                      axis=1,
                                      keep_dims=True)
@@ -97,21 +99,26 @@ class Agent(object):
             exp_v = ENTROPY_BETA * entropy + exp_v
             actor_loss = tf.reduce_mean(-exp_v)
 
+        tf.summary.scalar('actor_loss', tensor=actor_loss)
+        tf.summary.scalar('critic_loss', tensor=critic_loss)
+
         return actor_loss, critic_loss
 
     # 计算Loss Function的梯度
     @staticmethod
     def _get_loss_gradient(actor_loss, actor_params, critic_loss, critic_params):
-        with tf.name_scope('local_grad'):
-            actor_grads = tf.gradients(actor_loss, actor_params)
-            critic_grads = tf.gradients(critic_loss, critic_params)
+        with tf.name_scope('local_gradient'):
+            actor_grads = tf.gradients(actor_loss, actor_params, name='actor_gradient')
+            critic_grads = tf.gradients(critic_loss, critic_params, name='critic_gradient')
+
         return actor_grads, critic_grads
 
     # 搭建Actor和Critic网络
-    def _get_network_output(self, scope):
+    @staticmethod
+    def _get_network_output(state, scope):
         # 参数初始化工作非常重要
         w_init = tf.contrib.layers.xavier_initializer()
-        state = tf.reshape(self.state, [-1, 4 * 4 * 1])
+        state = tf.reshape(state, [-1, 4 * 4 * 1])
 
         # Actor网络
         with tf.variable_scope('actor'):
@@ -143,6 +150,7 @@ class Agent(object):
                                               units=1,
                                               kernel_initializer=w_init,
                                               name='estimated_value')
+            # tf.summary.scalar('estimated_value', estimated_value)
 
         # Actor-Critic的参数
         actor_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
@@ -200,6 +208,7 @@ class Worker(object):
                 current_episode_time += 1
 
             SCORE.append(np.sum(state))
+            tf.summary.scalar('score', tf.constant(np.sum(state)))
 
             # 得到最后一个state的Target Value
             if is_game_over:
@@ -210,14 +219,22 @@ class Worker(object):
                                         feed_dict={self.AC.state: state[np.newaxis]})[0, 0]
 
             buf_target_value = Worker._get_target_value_list(reward_list=buf_reward, last_target_value=target_value)
-
-            # 同步数据
-            self.AC.update_global(feed_dict={
+            buf_action = np.vstack(buf_action)
+            feed_dict = {
                 self.AC.state: buf_state,
                 self.AC.action: buf_action,
-                self.AC.target_value: buf_target_value,
-            })
+                self.AC.target_value: buf_target_value
+            }
+            # 同步数据
+            self.AC.update_global(feed_dict=feed_dict)
             self.AC.pull_global()
+
+            summary = SESS.run(merged, feed_dict={
+                self.AC.state: buf_state[0][np.newaxis],
+                self.AC.action: buf_action[0][np.newaxis],
+                self.AC.target_value: buf_target_value[0][np.newaxis]
+            })
+            writer.add_summary(summary, current_episode_time)
 
             print("EPISODE_TIME: %d" % current_episode_time)
 
@@ -258,14 +275,17 @@ if __name__ == "__main__":
     if OUTPUT_GRAPH:
         if os.path.exists(LOG_DIR):
             shutil.rmtree(LOG_DIR)
-        tf.summary.FileWriter(LOG_DIR, SESS.graph)
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter(LOG_DIR, SESS.graph)
 
-    worker_threads = []
-    for worker in workers:
-        job = lambda: worker.work()
-        t = threading.Thread(target=job)  # 添加一个工作线程
-        t.start()
-        worker_threads.append(t)
+    with tf.device('/gpu:0'):
+        worker_threads = []
+        for worker in workers:
+            job = lambda: worker.work()
+            t = threading.Thread(target=job)  # 添加一个工作线程
+            t.start()
+            worker_threads.append(t)
+
     # tf 的线程调度
     COORD.join(worker_threads)
 
