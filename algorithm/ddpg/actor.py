9796905ada5f3ff94f6f2e54f56f17ab85108f1e
Lance <nevertiree@126.com>
2017-12-29 18:39:25 +0800

    用卷积网络重写Action Net

--- algorithm/ddpg/actor.py
+++ algorithm/ddpg/actor.py
@@ -17,17 +17,16 @@ class Actor(Agent):
         self.log_dir = 'log/actor'
 
         with tf.variable_scope('Actor'):
-            self.estimate_action = self.actor_network('Estimate')
+            self.estimate_action = self.actor_network()
             self.estimate_para = tf.get_collection(key=tf.GraphKeys.GLOBAL_VARIABLES,
                                                    scope='Actor/Network')
 
         with tf.variable_scope('Target_Actor'):
-            self.target_action = self.actor_network('Target')
-            self.target_para = tf.get_collection(key=tf.GraphKeys.GLOBAL_VARIABLES,
-                                                 scope='Target_Actor/Network')
+            self.target_action = self.actor_network()
+            self.target_para = self.estimate_para
 
         with tf.variable_scope('Actor/Update'):
-            self.optimizer = tf.train.RMSPropOptimizer(0.01)
+            self.optimizer = tf.train.RMSPropOptimizer(0.001)
             self.action_gradient = tf.gradients(ys=self.q_value,
                                                 xs=self.estimate_action)
             self.policy_gradient = tf.gradients(ys=self.estimate_action,
@@ -40,7 +39,7 @@ class Actor(Agent):
             self.target_update = [tf.assign(ref=t, value=self.tau * t + (1 - self.tau) * e)
                                   for t, e in zip(self.target_para, self.estimate_para)]
 
-        with tf.variable_scope("C_Summary"):
+        with tf.variable_scope("A_Summary"):
             self.merge = tf.summary.merge_all()
             if os.path.exists(self.log_dir):
                 shutil.rmtree(self.log_dir)
@@ -49,36 +48,41 @@ class Actor(Agent):
 
         self.sess.run(tf.global_variables_initializer())
 
-    def actor_network(self, network_type):
-        if network_type == 'Estimate':
-            trainable = True
-        else:
-            trainable = False
+    def actor_network(self):
 
         with tf.variable_scope('Network', reuse=tf.AUTO_REUSE):
             init_w = tf.random_normal_initializer(mean=1, stddev=2)
             init_b = tf.constant_initializer(value=1)
 
             input_layer = tf.reshape(tensor=self.state,
-                                     shape=[-1, self.state_size * self.state_size],
+                                     shape=[-1, self.state_size, self.state_size, 1],
                                      name='Input_Layer')
-            first_layer = tf.layers.dense(inputs=input_layer,
-                                          units=16,
-                                          activation=tf.nn.relu,
-                                          kernel_initializer=init_w,
-                                          bias_initializer=init_b,
-                                          trainable=trainable,
-                                          name='First_Layer')
-            output_layer = tf.layers.dense(inputs=first_layer,
-                                           units=self.action_size,
-                                           activation=tf.nn.softmax,
-                                           kernel_initializer=init_w,
-                                           bias_initializer=init_b,
-                                           trainable=trainable,
-                                           name='Output')
-            output_layer = tf.reshape(tensor=output_layer,
-                                      shape=[-1, self.action_size])
-            return output_layer
+
+            conv_layer_1 = tf.layers.conv2d(inputs=input_layer,
+                                            filters=32,
+                                            kernel_size=[2, 2],
+                                            padding='valid',
+                                            kernel_initializer=init_w,
+                                            bias_initializer=init_b,
+                                            activation=tf.nn.relu)
+
+            conv_layer_2 = tf.layers.conv2d(inputs=conv_layer_1,
+                                            filters=64,
+                                            kernel_size=[2, 2],
+                                            padding='valid',
+                                            kernel_initializer=init_w,
+                                            bias_initializer=init_b,
+                                            activation=tf.nn.relu)
+
+            reshape = tf.reshape(tensor=conv_layer_2, shape=[-1, 2 * 2 * 64])
+
+            output = tf.layers.dense(inputs=reshape,
+                                     units=self.action_size,
+                                     activation=tf.nn.softmax,
+                                     kernel_initializer=init_w,
+                                     bias_initializer=init_b)
+
+            return output
 
     def get_action(self, s, network_type):
         s = s[np.newaxis]
@@ -88,7 +92,6 @@ class Actor(Agent):
             action_prob_list = self.sess.run(self.target_action, feed_dict={self.state: s})
         else:
             action_prob_list = self.sess.run(self.estimate_action, feed_dict={self.state: s})
-
         action = [np.random.choice(a=range(action_prob.shape[0]), p=action_prob.ravel())
                   for action_prob in action_prob_list]
 
